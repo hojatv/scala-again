061f0134b7a4f73121f576d8a8402c895f7f6dbc
hojatv <hojat.vaheb@gmail.com>
2021-05-29 18:07:59 +0200

    basics

--- src/main/scala/lectures/part1as/Recap.scala
+++ src/main/scala/lectures/part1as/Recap.scala
@@ -1,12 +1,30 @@
 package lectures.part1as
 
+import scala.annotation.tailrec
+
 object Recap extends App {
 
-  val aCondition : Boolean = false
-  val aConditionedVal = if(aCondition) 1 else 2
+  val aCondition: Boolean = false
+  val aConditionedVal = if (aCondition) 1 else 2
   //instructions vs expressions
   val aCodedBlock = {
-    if(aCondition) 3
+    if (aCondition) 3
     4
   }
+
+  //Unit = void
+  val theUnit = println("something!")
+
+  //functions
+  def aFunction(x: Int): Int = x + 1
+
+  println(aFunction(4))
+
+  //recursion: stack and tail
+  @tailrec def factorial(n: Int, accumulator: Int): Int =
+    if (n <= 0) accumulator
+    else factorial(n - 1, n * accumulator)
+
+  println(factorial(4, 1))
+
 }
